afc2162989ac6629667595df710744294cc7638e
Riley <rileykeycleary@yahoo.com>
2017-07-18 10:44:40 -0700

    Add bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,4 @@
+#Regular bubble sort
 def bubble_sort array
 	i=0
 	array.length.times do 
@@ -7,6 +8,37 @@ def bubble_sort array
 		end
 		i=0
 	end
-	puts array
+	array
 end
-bubble_sort([4,3,78,2,0,2])
+
+#Bubble sort that accepts a block and sorts by string length
+def bubble_sort_by array
+	i=0
+	array.length.times do
+		while i<array.length-1		
+			array[i],array[i+1]=array[i+1],array[i] if yield(array[i],array[i+1])>0
+			i+=1
+		end
+		i=0
+	end
+	array
+end
+
+#Method used to create and populate a random array
+def array_gen(size)
+	array=[]
+	size.times do
+		array.push(rand(1..20))
+	end
+	puts array.join(", ")
+	return array
+end
+
+#Testing bubble_sort
+puts bubble_sort(array_gen(12)).join(", ")
+
+#Testing bubble_sort_by
+sorted_words = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+	left.length-right.length
+end
+puts sorted_words.join(", ")
